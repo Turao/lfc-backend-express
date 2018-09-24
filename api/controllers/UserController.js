6eacc818d6411b844509d8b2a200556a8b0d2577
turao <lenz.arthur@gmail.com>
2018-09-24 06:14:17 -0300

    changing to promises on UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,20 +5,15 @@ let UserController = {
   get: function (req, res) {
     console.log('getting user of id:', req.params.id);
     
-    UserModel.findById(req.params.id, (err, user) => {
-      if (err) {
+    UserModel.findById(req.params.id)
+      .exec()
+      .then( user => {
+        if (!user) res.sendStatus(404); // not found
+        else res.json(user);
+      })
+      .catch( err => {
         res.sendStatus(400); // bad request
-        return
-      }
-
-      if (!user) { 
-        res.sendStatus(404);
-        return // not found
-      }
-
-      res.json(user);
-      return
-    });
+      })
   },
 
 
@@ -26,15 +21,13 @@ let UserController = {
     console.log('creating user:', req.body.user);
 
     let user = req.body.user;
-    UserModel.create(user, (err, user) => {
-      if(err) { 
+    UserModel.create(user)
+      .then( user => {
+        res.sendStatus(200); // ok
+      })
+      .catch( err => {
         res.sendStatus(400); // bad request
-        return
-      }
-
-      res.sendStatus(200); // ok
-      return
-    });
+      })
   },
 
 
@@ -42,30 +35,26 @@ let UserController = {
     console.log('updating user of id:', req.params.id);
     
     let user = req.body.user;
-    UserModel.updateOne({_id: req.params.id}, user, (err, user) => {
-      if(err) { 
-        res.sendStatus(400); // bad request
-        return
-      }
-      
+    UserModel.updateOne({_id: req.params.id}, user).exec()
+    .then( user => {
       res.sendStatus(200); // ok
-      return
-    });
+    })
+    .catch( err => {
+      res.sendStatus(400); // bad request
+    })
   },
 
   
   remove: function (req, res) {
     console.log('removing user of id:', req.params.id);
     
-    UserModel.remove({_id: req.params.id}, (err, user) => {
-      if(err) { 
-        res.sendStatus(400); // bad request
-        return
-      }
-      
+    UserModel.remove({_id: req.params.id}).exec()
+    .then( user => {
       res.sendStatus(200); // ok
-      return
-    });
+    })
+    .catch( err => {
+      res.sendStatus(400); // bad request
+    })
   },
 
 
