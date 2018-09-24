93d3dc0fdce74f9cc5ff710905063c5764eff1d9
turao <lenz.arthur@gmail.com>
2018-09-24 19:55:38 -0300

    refactor auth to use async/await and reduce nesting

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -7,41 +7,37 @@ const UserModel = require('../models/UserModel');
 
 let AuthController = {
 
-  signup: function (req, res) {
-    bcrypt.hash(req.body.user.password, 10)
-    .then( hash => {
+  signup: async function (req, res) {
+    try {
+      let hash = await bcrypt.hash(req.body.user.password, 10);
       req.body.user.password = hash;
       UserController.create(req, res);
-    })
-    .catch( err => { 
+    } catch (err) {
       res.sendStatus(500); // internal error
-      })
-    },
+    }
+  },
 
-    login: function (req, res) {
-      UserModel.findOne({email: req.body.user.email})
-      .exec()
-      .then( user => {
-        bcrypt.compare(req.body.user.password, user.password)
-        .then( result => {
-          if(result) {
-            // create jwt token
-              let __superPrivateSecretKey = 'geromito';
-              const JWTToken = jwt.sign({
-                _id: user._id,
-                email: user.email,
-              }, __superPrivateSecretKey, {'expiresIn': '2h'});
-              
-              res.status(200).json({
-                token: JWTToken,
-              });
-          }
-          else res.sendStatus(401); // unauthorized access
-        })
-      })
-      .catch( err => {
-        res.sendStatus(401); // unauthorized access
-      })
+    login: async function (req, res) {
+      try {
+        let user = await UserModel.findOne({email: req.body.user.email}).exec();
+        let isSamePassword = await bcrypt.compare(req.body.user.password, user.password);
+        if (isSamePassword) {
+          let __superPrivateSecretKey = 'geromito';
+          const JWTToken = jwt.sign({
+            _id: user._id,
+            email: user.email,
+          }, __superPrivateSecretKey, {'expiresIn': '2h'});
+          
+          res.status(200).json({
+            token: JWTToken,
+          });
+        }
+        else {
+          throw Error('Unauthorized Access'); // unauthorized access
+        }
+      } catch (err) {
+        res.sendStatus(401); // unauthorized access 
+      }
   },
 
   authorize: function (req, res, next) {
